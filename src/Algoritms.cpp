683424b38fdad099d3697f6ee7f5def6075865d8
llisnikowski <l.lisnikowski@gmail.com>
2024-03-16 17:46:10 +0100

    Add pow function

--- src/Algoritms.cpp
+++ src/Algoritms.cpp
@@ -4,3 +4,12 @@ float add(float lhs, float rhs)
 {
     return lhs + rhs;
 }
+
+float pow(float base, unsigned int exp)
+{
+    float result = 1.f;
+    for(int i = 0; i < exp; i++){
+        result *= base;
+    }
+    return result;
+}
--- src/Algoritms.hpp
+++ src/Algoritms.hpp
@@ -1,3 +1,5 @@
 #pragma once
 
 float add(float lhs, float rhs);
+
+float pow(float base, unsigned int exp);
